ff7c078c6eb086b1e61e9ce8ed06949c04140a13
Rayan <rayan.mahm08@gmail.com>
2024-12-02 23:38:01 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+print("1. Celsius to Fahrenheit")
+print("2. Fahrenheit to Celsius")
+
+choice = input("Choose an option to convert (1/2): ")
+
+if choice == "1":
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius*9/5)+32
+    print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
+elif choice == "2":
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit-32)*5/9
+    print(f"{fahrenheit}°F is equal to {celsius:.2f}°C")
+else:
+    print("Invalid choice. Please select 1 or 2.")
